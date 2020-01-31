96b07f6f4878d8e4cf877ffadd893854e161ca78
Simon Jewell <s.jewell@parcelvision.com>
2020-01-31 12:01:32 +0000

    PVA-324 remove old tests

--- services-list.cmd
+++ services-list.cmd
@@ -1,12 +1,38 @@
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-elasticsearch.yml^
+ REM -f compose-eventstore.yml^
+ REM -f compose-kibana.yml^
+ REM -f compose-mongo.yml^
+ REM -f compose-rabbitmq.yml^
+ REM -f compose-vault.yml^
+ REM -f network.yml
+ 
+ REM **** WILLOW ****
 SET DOCKER_DEVSVC_PROJECT=devsvc
 SET DOCKER_DEVSVC_COMPOSE_FILES=^
  -f compose-elasticsearch.yml^
  -f compose-eventstore.yml^
- -f compose-kibana.yml^
  -f compose-mongo.yml^
  -f compose-rabbitmq.yml^
  -f network.yml
+ 
+ REM **** POPLAR ****
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-elasticsearch.yml^
+ REM -f compose-eventstore.yml^
+ REM -f compose-rabbitmq.yml^
+ REM -f compose-postgres.yml^
+ REM -f network.yml 
 
+ REM **** ROWAN ****
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-elasticsearch.yml^
+ REM -f compose-rabbitmq.yml^
+ REM -f compose-postgres.yml^
+ REM -f network.yml
 
 REM Add to the list above as required
 REM -f compose-elasticsearch.yml^
